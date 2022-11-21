37784001983d8863def281786be702c2b0610ade
e-sivertsen <e.sivertsen@hotmail.com>
2022-11-21 16:28:34 +0100

    Implement Constructor Test

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -8,11 +8,12 @@
 #include "../ManagerInterface.h"
 
 
-TEST(myTests, test1) {
-        ClassProject::Manager* myTest;
-        myTest->testval(1);
-        ASSERT_EQ(myTest->testval(), 1) << "a is not 1";
-    }
+TEST(managerTests, constructorTest)
+{
+    ClassProject::Manager* testObj = new ClassProject::Manager();
+    ASSERT_EQ(testObj->uniqueTable[0].high, 0) << "Unique table does not contain false on construction";
+    ASSERT_EQ(testObj->uniqueTable[1].low, 1) << "Unique table does not contain true on construction";
+}
 
 int main(int argc, char* argv[])
 {
