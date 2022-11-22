dba4c65a9b645c55b692b1e1e330ce5028d7a4d9
e-sivertsen <e.sivertsen@hotmail.com>
2022-11-22 11:46:35 +0100

    Implement trivial test

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -15,6 +15,24 @@ TEST(managerTests, constructorTest)
     ASSERT_EQ(testObj->uniqueTable[1].low, 1) << "Unique table does not contain true on construction";
 }
 
+TEST(managerTests, createVarTest)
+{
+    ClassProject::Manager* testObj = new ClassProject::Manager();
+    testObj->createVar("testVar");
+    ASSERT_EQ(testObj->uniqueTable[2].id, 3) << "Did not create";
+    ASSERT_EQ(testObj->uniqueTable[2].label, "testVar") << "Did not create";
+}
+
+TEST(managerTests, trivialTest)
+{
+    ClassProject::Manager* testObj = new ClassProject::Manager();
+    testObj->createVar("testVar");
+    ASSERT_EQ(testObj->uniqueTableSize(),testObj->uniqueTable.size()) << "Wrong size";
+    ASSERT_EQ(testObj->True(), 2) << "Wrong id for True";
+    ASSERT_EQ(testObj->False(), 1) << "Wrong id for False";
+    ASSERT_EQ(testObj->isVariable(3), true) << "Not a variable";
+}
+
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
