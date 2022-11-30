5326831587424fceb8c5ed83edf56168f9f495ad
e-sivertsen <e.sivertsen@hotmail.com>
2022-11-30 20:00:54 +0100

    Test implementations

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -1,41 +1,66 @@
-//
-// Created by ludwig on 22.11.16.
-//
-
-
 #include "Tests.h"
-#include "../Manager.h"
-#include "../ManagerInterface.h"
 
-
-TEST(managerTests, constructorTest)
+class managerTest : public ::testing::Test 
 {
-    ClassProject::Manager* testObj = new ClassProject::Manager();
-    ASSERT_EQ(testObj->uniqueTable[0].high, 0) << "Unique table does not contain false on construction";
-    ASSERT_EQ(testObj->uniqueTable[1].low, 1) << "Unique table does not contain true on construction";
+    protected:
+        ClassProject::Manager testObj;
+        ClassProject::BDD_ID a, b, c, d;
+
+        void SetUp() override {
+            a = testObj.createVar("a");
+            b = testObj.createVar("b");
+            c = testObj.createVar("c");
+            d = testObj.createVar("d");
+        }
+
+        ClassProject::BDD_ID runExpression()
+        {
+            return testObj.and2(testObj.and2(c,d),testObj.or2(a,b));
+        }
+};      
+
+TEST_F(managerTest, constructorTest)
+{
+    ASSERT_EQ(testObj.uniqueTable[testObj.False()].label, "False") << "Unique table does not contain false on construction";
+    ASSERT_EQ(testObj.uniqueTable[testObj.True()].label, "True") << "Unique table does not contain true on construction";
 }
 
-TEST(managerTests, createVarTest)
+TEST_F(managerTest, createVarTest)
 {
-    ClassProject::Manager* testObj = new ClassProject::Manager();
-    testObj->createVar("testVar");
-    ASSERT_EQ(testObj->uniqueTable.back().label, "testVar") << "Variable was not created";
+    ClassProject::BDD_ID testVar = testObj.createVar("testVar");
+    ASSERT_EQ(testObj.uniqueTable[testVar].label, "testVar") << "Incorrect creation of variable";
 }
 
-TEST(managerTests, trivialTest)
+TEST_F(managerTest, uniqueTableSizeTest)
 {
-    ClassProject::Manager* testObj = new ClassProject::Manager();
-    ClassProject::BDD_ID testVar = testObj->createVar("testVar");
-    ASSERT_EQ(testObj->uniqueTableSize(),testObj->uniqueTable.size()) << "Wrong size";
-    ASSERT_EQ(testObj->True(), 1) << "Wrong id for True";
-    ASSERT_EQ(testObj->False(), 0) << "Wrong id for False";
-    ASSERT_EQ(testObj->isVariable(testVar), true) << "Incorrect assesment of variable";
+    size_t preSize = testObj.uniqueTableSize();
+    ClassProject::BDD_ID testVar = testObj.createVar("testVar");
+    ASSERT_EQ(testObj.uniqueTableSize(), preSize + 1) << "Incorrect assertion for size";
+}
+
+TEST_F(managerTest, isConstantTest)
+{
+    for (ClassProject::BDD_ID i = 0; i < testObj.uniqueTableSize(); i++) 
+    {
+        bool res = (i == testObj.False() || i == testObj.True());
+        ASSERT_EQ(testObj.isConstant(i), res) << "Incorrect assertion for isConstant";
+    }
+}
+
+TEST_F(managerTest, isVariableTest)
+{
+    runExpression();
+    ClassProject::BDD_ID i = 0;
+    for (auto item : testObj.uniqueTable)
+    {
+        bool res = (item.label == "a" || item.label == "c" || item.label == "b" || item.label == "d");
+        ASSERT_EQ(testObj.isVariable(i++), res) << "Incorrect assertion for isVariable";
+    }
 }
 
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
-
 
     return RUN_ALL_TESTS();
 }
