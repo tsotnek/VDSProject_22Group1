572c59d022cd7c2b59181762e4c9ddc3c2190ec8
e-sivertsen <e.sivertsen@hotmail.com>
2023-01-18 12:35:42 +0100

    Part 3 init

---
+++ src/reachability/Reachability.h
@@ -0,0 +1,13 @@
+#ifndef VDSPROJECT_REACHABILITY_H
+#define VDSPROJECT_REACHABILITY_H
+
+#include "ReachabilityInterface.h"
+
+namespace ClassProject {
+
+//    class Reachability : public ReachabilityInterface {
+//
+//    };
+
+}
+#endif
