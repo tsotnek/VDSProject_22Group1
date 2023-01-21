ed5e6f57660b7ab51fd2c2989f180f35c485342e
e-sivertsen <e.sivertsen@hotmail.com>
2023-01-21 19:04:48 +0100

    Reachability basics

--- src/reachability/Reachability.cpp
+++ src/reachability/Reachability.cpp
@@ -1,1 +1,39 @@
 #include "Reachability.h"
+
+#include <algorithm>
+
+using namespace ClassProject;
+
+const std::vector<BDD_ID> &Reachability::getStates() const
+{
+    return states;
+}
+
+bool Reachability::isReachable(const std::vector<bool> &stateVector) 
+{
+    if (stateVector.size() != states.size())
+        std::__throw_runtime_error("Incorrect size");
+    
+}
+
+void Reachability::setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) 
+{
+    if (transitionFunctions.size() != states.size())
+        std::__throw_runtime_error("Incorrect size");
+    transitions = transitionFunctions;
+}
+
+void Reachability::setInitState(const std::vector<bool> &stateVector) 
+{
+    if (stateVector.size() != states.size())
+        std::__throw_runtime_error("Incorrect size");
+    initialState = stateVector;
+}
+
+explicit Reachability::Reachability(unsigned int stateSize) : ReachabilityInterface::ReachabilityInterface(stateSize)
+{
+    if (stateSize == 0)
+        std::__throw_runtime_error("Size cannot be zero");
+    for (unsigned int i = 0; i < stateSize; i++ )
+        states.push_back(createVar("s" + std::to_string(i)));
+}
--- src/reachability/Reachability.h
+++ src/reachability/Reachability.h
@@ -5,9 +5,28 @@
 
 namespace ClassProject {
 
-//    class Reachability : public ReachabilityInterface {
-//
-//    };
+    class Reachability : public ReachabilityInterface 
+    {
+        private:
+        
+            std::vector<BDD_ID> states;
 
-}
+            std::vector<BDD_ID> transitions;
+
+            std::vector<bool> initialState;
+
+        public:
+
+            const std::vector<BDD_ID> &getStates() const override;
+
+            bool isReachable(const std::vector<bool> &stateVector) override;
+
+            void setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) override;
+
+            void setInitState(const std::vector<bool> &stateVector) override;
+
+            explicit Reachability(unsigned int stateSize);
+    };
+
+};
 #endif
